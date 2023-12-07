349b5f0257340d08e844f33ee6c50bce99cf8da2
GR4CE <gr4ce001@gmail.com>
2023-12-07 22:09:27 +0530

    added routes ad pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
 
 export default function App() {
   return (
-    <div className='bg-red-200'>App</div>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/sign-in' element={<Signin/>} />
+    <Route path='/sign-up' element={<Signup/>} />
+    <Route path='/profile' element={<Profile/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }

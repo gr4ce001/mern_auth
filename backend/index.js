53eea529099c24abcdc7262785c4cb80802b0d1a
GR4CE <gr4ce001@gmail.com>
2023-12-07 23:18:57 +0530

    connected db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
+mongoose.connect(process.env.MONGO_CONNECT).then(() => {
+    console.log(`Connected to MongoDB`);
+});
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});

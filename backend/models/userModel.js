b3112acb5d12db90956d1ea9ef9efcc082063ef3
GR4CE <gr4ce001@gmail.com>
2023-12-08 09:42:14 +0530

    added user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    mobile:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+},
+{
+    timestamps:true,
+});
+
+const User = mongoose.model('User', userSchema);
+//Export the model
+module.exports = User;

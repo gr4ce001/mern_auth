1a84e5f83d5be743bd3e4dc22cbf829e0c393ce5
GR4CE <gr4ce001@gmail.com>
2023-12-09 04:42:08 +0530

    bug fix

--- backend/controllers/authCtrl.js
+++ backend/controllers/authCtrl.js
@@ -4,9 +4,9 @@ const errorHandler = require("../utils/error.js")
 const jwt = require('jsonwebtoken')
 
 const signup = async (req, res, next) => {
-  const { username, email, password } = req.body;
+  const { name, email,mobile, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
+  const newUser = new User({ name, email,mobile, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json({ message: 'User created successfully' });
